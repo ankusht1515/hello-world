99e903497fe401d214e74dc0c2aea878771db0d7
“ankusht1515” <ankusht1515@gmail.com>
2019-01-16 12:36:57 +0530

    First commit

---
+++ sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupal8_demo',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = '2scL46J38iV8vPBsP3XJMGGm4ECzjK1dEuSkr6AYNHD8NEF14hvpyKzdhXAx70ZzqsoZkjbWzA';
+$config_directories['sync'] = 'sites/default/files/config_lVrE_62YNiGFuaAZ1AcYRNboq9jdddEoCqv0S1nBdA7P1rcVimK84SREBPx7JF57TnfoetoQEQ/sync';
